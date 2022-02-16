bcd97783be18f842d6ea980fc5aaf5a983d6abc0
Hossain-Sajjad <sunshinems58.com>
2022-02-17 03:23:26 +0600

    main part calculation added

--- js/script.js
+++ js/script.js
@@ -9,7 +9,9 @@ document.getElementById('calculate-button').addEventListener('click', function()
     const rentValue = getInputValue('rent');
     const clothesValue = getInputValue('clothes');
     const totalExpenses = parseInt(foodValue) + parseInt(rentValue) + parseInt(clothesValue);
-    
+    document.getElementById('total-expenses').innerText = totalExpenses;
+    document.getElementById('balance').innerText = parseInt(incomeValue) - totalExpenses;
+
     console.log(totalExpenses);
 })
 
